b98368aeebe55fce315533d30a4fb602338bc9c1
Batuhan Açan <batuhanacan@batus-Macbook.local>
2025-10-06 17:21:38 +0300

    backup

--- backend/ChatApi/Models/Message.cs
+++ backend/ChatApi/Models/Message.cs
@@ -7,12 +7,12 @@ namespace ChatApi.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Nickname { get; set; }  // rumuz
-        public string Text { get; set; }
+        public string? Nickname { get; set; }  // rumuz
+        public string? Text { get; set; }
         public DateTime CreatedAt { get; set; }
 
         // AI sonuçları
-        public string Sentiment { get; set; }   // "positive"|"neutral"|"negative"
+        public string? Sentiment { get; set; }   // "positive"|"neutral"|"negative"
         public double SentimentScore { get; set; }
     }
 }
